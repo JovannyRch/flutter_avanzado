6a1e69cef05d791fad2c4c16841438a8520089e2
JovannyRch <43225215+JovannyRch@users.noreply.github.com>
2020-05-20 18:14:14 -0500

    Header triangular

--- lib/widgets/headers_widgets.dart
+++ lib/widgets/headers_widgets.dart
@@ -22,8 +22,8 @@ class RoundedHeader extends StatelessWidget {
   }
 }
 
-class Header1 extends StatelessWidget {
-  const Header1({Key key}) : super(key: key);
+class HeaderDiagonal extends StatelessWidget {
+  const HeaderDiagonal({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +46,76 @@ class _MiPainter extends CustomPainter {
     final path = new Path();
     path.lineTo(0, size.height * 0.40);
     path.lineTo(size.width, size.height * 0.35);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class HeaderTriangulo extends StatelessWidget {
+  const HeaderTriangulo({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _MiPainter2(),
+      ),
+    );
+  }
+}
+
+class _MiPainter2 extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = new Paint();
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    final path = new Path();
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class HeaderPico extends StatelessWidget {
+  const HeaderPico({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _MiPainter3(),
+      ),
+    );
+  }
+}
+
+class _MiPainter3 extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = new Paint();
+    paint.color = Color(0xff615AAB);
+    paint.style = PaintingStyle.fill;
+    final path = new Path();
+    path.lineTo(size.width * 0.5, size.height * 0.5);
     path.lineTo(size.width, 0);
     path.lineTo(0, 0);
 
